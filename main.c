b71f7579797b04a75b06c7cfa9722d53f8db7047
Guzin Gunduz <guzgunduz@gmail.com>
2023-02-05 15:38:47 +0100

    #2 Create a Structures in C
    1-Before creating the structure variables, I defined the data type. The "struct "keyword is used to define a structure.
    2-When a struct type is declared, no storage or memory is allocated. We need to create variables to allocate memory of a particular build type and work with it. ---> I defined it human1,human2, and h[20] ;
    3. after that I created struct with human1 variable.

--- main.c
+++ main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
+struct human
 {
-    printf("Hegllo world!\n");
+    char name [25];
+    char city[25] ;
+    float  age ;
+} human1, human2, h[20];
+
+
+
+int main ()
+{
+    strcpy(human1.name, "Guzin");
+    human1.age = 29;
+    strcpy(human1.city,"Bursa");
+
+    printf("Name: %s\n", human1.name);
+    printf("City : %s\n", human1.city);
+    printf("Age : %.1f\n", human1.age);
+
+
+
+
+
     return 0;
 }
